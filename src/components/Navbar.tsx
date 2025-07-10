8e55b1ad901be606f627333b19072d0f0b7ef1a4
Noodles <yilangcharleshe@gmail.com>
2025-07-09 21:56:07 -0400

    Update Navbar component to conditionally display NavigationMenuItem on larger screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,7 +56,7 @@ export function Navbar() {
         </div>
         <NavigationMenu>
           <NavigationMenuList>
-            <NavigationMenuItem>
+            <NavigationMenuItem className="hidden sm:block">
               <NavigationMenuTrigger className="text-white hover:text-white/90 data-[active]:text-white data-[state=open]:text-white bg-transparent hover:bg-white/10 data-[state=open]:bg-white/10 px-3 py-2 rounded-md transition-colors">
                 Features
               </NavigationMenuTrigger>
